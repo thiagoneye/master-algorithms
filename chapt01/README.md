b4b50914d4924ea1490393fd5924a2f751441a89
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2022-11-07 22:04:19 -0300

    Upload files

---
+++ chapt01/README.md
@@ -0,0 +1,8 @@
+# Studied Contents
+
+1. 
+
+# List of Scripts
+
+1. Multiplication à la Français
+2. Division
---
+++ chapt01/script01.py
@@ -0,0 +1,30 @@
+# Auxiliary Functions
+
+def multiplication(x: int, y: int) -> int:
+    """
+    Multiplication à la Français.
+    """
+    if y == 0:
+        z = 0
+    else:
+        z = list()
+        
+        while x > 0:
+            if x % 2 != 0:
+                z.append(y)
+            
+            print(f'{x} | {y} | {z}')
+            
+            x = int(x/2)
+            y *= 2
+    
+    z = sum(z)
+    
+    return z
+
+# Execution
+
+print(multiplication(25, 37), '\n')
+print(multiplication(81, 102), '\n')
+print(multiplication(24, 31), '\n')
+print(multiplication(10, 20), '\n')
